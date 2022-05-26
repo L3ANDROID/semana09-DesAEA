b9144bcfe8c4ca5c1187648f46f1ab71dc77fafc
Leandro Gabriel <40245178+L3ANDROID@users.noreply.github.com>
2022-05-25 23:41:53 -0500

    Agregar archivos de proyecto.

---
+++ MVCExample2022/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MVCExample2022.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Desciption { get; set; }
+        public double Price { get; set; }
+    }
+}
